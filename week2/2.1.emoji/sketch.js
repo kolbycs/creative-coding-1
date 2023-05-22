93d062f8395f70341eb21dc8354e779030bee258
kolbycs <ksimpson@mail.umw.edu>
2023-05-22 14:31:05 -0400

    Update sketch.js

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -4,7 +4,7 @@ function setup() {
 
 function draw() {
   background(220);
-
+  ellipse(200,200,250,250);
   stroke("gold"); // set the stroke (line) color 
   strokeWeight(6); // set the stroke thickness
   fill("yellow"); // set the fill color
@@ -12,6 +12,6 @@ function draw() {
 // draw an ellipse with 250 pixel diameter
   // with its center at x position 200
   // and at y position 200
-  ellipse(200,200,250,250);
+  
 
 }
