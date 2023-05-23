d960e89b6231ae967371ce262f923518d5fc248b
kolbycs <ksimpson@mail.umw.edu>
2023-05-23 14:20:24 -0400

    Update sketch.js

--- week2/2.2.circles/sketch.js
+++ week2/2.2.circles/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   createCanvas(1200, 1200);
-noLoop()
+noLoop()    // stop circles from moving
 }
 
 function draw() {
@@ -10,7 +10,7 @@ translate(100,100);
 for (let x = 0; x < 8; x++){
    for (let y = 0; y < 8; y++){
       push();
-      translate(x * 100 + random(-50, 50), y * 100 + random(-50, 50)); // add randomness from a range of numbers
+      translate(x * 100 + random(-40, 40), y * 100 + random(-40, 40)); // add randomness from a range of numbers
         fill ('black');       //change inside color
       stroke('white');        //change outline color
       strokeWeight('40');     //change outline thickness
