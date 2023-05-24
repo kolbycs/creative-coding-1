bd39d2b477dc0aa84c424c65d00dd22618438c5c
kolbycs <ksimpson@mail.umw.edu>
2023-05-24 15:59:56 -0400

    Update sketch.js

--- week2/2.4.quilt-pattern/sketch.js
+++ week2/2.4.quilt-pattern/sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(2030, 2675);
+  createCanvas(2675, 2030);
   noLoop();
 }
 
@@ -11,8 +11,8 @@ https://www.internationalquiltmuseum.org/quilt/19970070181
 function draw() {
   background(0,0,140);
 
-  for (let x = 0; x < 15; x++) {      //makes a 15,15 grid
-    for (let y = 0; y < 20; y++) {
+  for (let x = 0; x < 20; x++) {      //makes a 20,15 grid
+    for (let y = 0; y < 15; y++) {
       push();
       translate(x * 130, y * 130);
            if ((x + y) % 2 == 0) {   //uses the sums of x and y and divides it by 2 to determine if odd or even. Uses the answer to assign a color scheme, creating a checkerboard pattern.
