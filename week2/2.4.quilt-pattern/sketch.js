1282fbc85146626285d16818124b065064610564
kolbycs <ksimpson@mail.umw.edu>
2023-05-24 13:04:53 -0400

    Update sketch.js

--- week2/2.4.quilt-pattern/sketch.js
+++ week2/2.4.quilt-pattern/sketch.js
@@ -1,7 +1,22 @@
 function setup() {
   createCanvas(400, 400);
+noLoop()
 }
 
 function draw() {
   background(220);
+  
+  angleMode(DEGREES);
+
+for (let x = 0; x < 10; x++){
+  for (let y = 0; y < 10; y++){
+    push();
+    translate(x * 100, y * 100);
+    
+    rect(50,50,100,100)
+    // tile drawing code here
+
+    pop();
+  }
+}
 }
