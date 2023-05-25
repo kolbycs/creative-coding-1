699edbd22c0d61a50f0a2009eb615a6b5f79c8a9
kolbycs <ksimpson@mail.umw.edu>
2023-05-25 15:10:14 -0400

    Update sketch.js

--- week2/2.3.emulation/sketch.js
+++ week2/2.3.emulation/sketch.js
@@ -1,20 +1,27 @@
+let colors = [     //create a list of colors in a global variable
+  ["#4061E5"],
+  ["#3a4ecd"],
+  ["#596bb0"],
+  ["#353d66"],
+  ["#3d79cd"],
+  ["#3979b8"],
+];
+
 function setup() {
-  createCanvas(1325, 660);
-noLoop()
+  createCanvas(1325, 660);    
+noLoop()  
 }
 
 function draw() {
-  background("#161618");
-
+  background("#161618");  //change background color
   translate(60,60);
-for (let x = 0; x < 6; x++){
+for (let x = 0; x < 6; x++){      //create 6 by 3 grid
    for (let y = 0; y < 3; y++){
       push();
-      translate(x * 220,y * 220); //change spacing of circles
-      fill (random(0,144),100,144);       //change inside color
-      stroke(random (144,255),200,255);        //change outline color randomly
-      strokeWeight(10);     //change outline thickness
-     rect(0,0,100,100);
+      translate(x * 220,y * 220); //change spacing of squares
+    let colorlist = random(colors); //create a local variable that randomizes color list
+     fill (colorlist);   //fill with randomizes color list
+     rect(0,0,100,100);  //create a small rectangle 
       pop();
    }
 
