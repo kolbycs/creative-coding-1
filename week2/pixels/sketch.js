9c51395d8a56d75e72c3932cde497d2aff382b18
kolbycs <ksimpson@mail.umw.edu>
2023-05-27 14:06:05 -0400

    Update sketch.js

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,4 +1,6 @@
 let img;
+let sampleX = 50;
+let sampleY = 50;
 function preload(){
    img = loadImage('https://kolbycs.github.io/creative-coding-true/week2/pixels/chief.jpg');
 }
@@ -10,4 +12,20 @@ function setup() {
 
 function draw() {
   background(220);
+
+for (let x = 0; x <= img.width; x += sampleX ) {
+
+for (let y = o; y <= img.height; y += sampleY )
+
+var index = (x + y * img.width) * 4;
+
+var r = img.pixels[index + 0];
+var g = img.pixels[index + 1];
+var b = img.pixels[index + 2];
+
+fill(r,g,b);
+ellipse(x,y,sampleX,sampleY);
+
+}
+
 }
