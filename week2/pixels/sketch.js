ae59067215ab0f18eac8c850ba92a7f89bf50baf
kolbycs <ksimpson@mail.umw.edu>
2023-05-27 13:44:18 -0400

    Update sketch.js

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,5 +1,11 @@
+let img;
+function preload(){
+   img = loadImage('https://kolbycs.github.io/creative-coding-true/week2/pixels/chief.jpg');
+}
+
 function setup() {
   createCanvas(800, 800);
+  img.loadPixels();
 }
 
 function draw() {
