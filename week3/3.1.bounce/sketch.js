5d9e176bfff8b85e1c43cdb49dcb35aea3fd6164
kolbycs <ksimpson@mail.umw.edu>
2023-05-30 14:54:36 -0400

    Update sketch.js

--- week3/3.1.bounce/sketch.js
+++ week3/3.1.bounce/sketch.js
@@ -10,12 +10,12 @@ function setup() {
 function draw() {
   background(220);
 
-  // check for collision with left and right edge
+  // check for collision with left and right edge, top and bottom edge
   if (x < 0 | x > width){
     xspeed = xspeed * -1; // reverse the x direction
 }
   if (y < 0 | y > height){
-    yspeed = yspeed * -1; // reverse the x direction
+    yspeed = yspeed * -1; // reverse the y direction
   }
   x = x + xspeed; // iterate x
   y = y + yspeed; // iterate y
