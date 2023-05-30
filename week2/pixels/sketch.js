edb80e00c91b1f488b474c6687d515a7dd120dd6
kolbycs <ksimpson@mail.umw.edu>
2023-05-30 17:20:24 -0400

    Update sketch.js

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,18 +1,18 @@
 let img;
-let sampleX = 30;
+let sampleX = 30; //change size of pixels
 let sampleY = 30;
 function preload(){
-   img = loadImage('https://kolbycs.github.io/creative-coding-true/week2/pixels/chief.jpg');
+   img = loadImage('https://kolbycs.github.io/creative-coding-true/week2/pixels/chief.jpg'); //load image 
 }
 
 function setup() {
   createCanvas(800, 800);
-  img.loadPixels();
+  img.loadPixels();  //load image data
 }
 
 function draw() {
   background(220);
-  noStroke();
+  noStroke(); //hide outline
   
 for (let x = 0; x <= img.width; x += sampleX ) {
 
@@ -24,9 +24,9 @@ var r = img.pixels[index + 0];
 var g = img.pixels[index + 1];
 var b = img.pixels[index + 2];
 
-fill(r,g,b);
-//rect(x,y,sampleX,sampleY);
-triangle(x, y, x + sampleX, y, x + sampleX / 2, y + sampleY);
+fill(r,g,b); //fill with color data 
+
+triangle(x, y, x + sampleX, y, x + sampleX / 2, y + sampleY); //create triangles
 }
 
 }
