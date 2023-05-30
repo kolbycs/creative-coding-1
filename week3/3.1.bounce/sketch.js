7bc386a40bff9121ecacc0849cb5119c76cd10d3
kolbycs <ksimpson@mail.umw.edu>
2023-05-30 13:55:39 -0400

    Update sketch.js

--- week3/3.1.bounce/sketch.js
+++ week3/3.1.bounce/sketch.js
@@ -1,7 +1,22 @@
+let x = 0;
+let y = 0;
+let xspeed = 1;
+let yspeed = 1;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+
+  // check for collision with left and right edge
+  if (x < 0 | x > width){
+    xspeed = xspeed * -1; // reverse the x direction
 }
+  x = x + xspeed; // iterate x
+  y = y + yspeed; // iterate y
+  
+  circle(x, y, 50);
+  }
+
