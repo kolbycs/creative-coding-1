777ba109d0fb0cfd53c1afc110edfa66d6e9a59f
kolbycs <ksimpson@mail.umw.edu>
2023-05-31 15:59:07 -0400

    Update sketch.js

--- week3/3.1.bounce/sketch.js
+++ week3/3.1.bounce/sketch.js
@@ -20,18 +20,18 @@ function draw() {
   background('green');
 
   // check for collision with left and right edge, top and bottom edge
-  if (x < 0 || x > width-img.width){
-    xspeed *= -1; // reverse the x direction
+  if (x < 0 || x > width){
+    xspeed = xspeed * -1; // reverse the x direction
     let textslist = random(texts);  //call back to array, randomize
     displayText(textslist);
   }
-  if (y < 0 || y > height-img.height){
-    yspeed *= -1; // reverse the y direction
+  if (y < 0 || y > height){
+    yspeed = yspeed * -1; // reverse the y direction
     let textslist = random(texts);
     displayText(textslist);
   }
-  x += xspeed; // iterate x
-  y += yspeed; // iterate y
+  x = x + xspeed; // iterate x
+  y = y + yspeed; // iterate y
   
   image(img,x,y,80,80);
   
@@ -44,5 +44,7 @@ function draw() {
   
     text(message, width / 2, height / 2); // display text in center of canvas
   }
+  
+
   
 
