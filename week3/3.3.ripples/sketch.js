15e25eb314c6039b1d7c39abe0929207a85806f0
kolbycs <ksimpson@mail.umw.edu>
2023-06-01 14:58:22 -0400

    Update sketch.js

--- week3/3.3.ripples/sketch.js
+++ week3/3.3.ripples/sketch.js
@@ -2,17 +2,18 @@ let ripple_x = 0;
 let ripple_y = 0;
 let ripple_d = 0;
 let ripplers = [];
-class Rippler {
-  constructor (x,y){
-   this.x = x,
-   this.y = y,
-   this.d = 0
-  }
-draw(){
-  this.d += 1;
-  circle(this.x, this.y, this.d);
-}
 
+class Rippler {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.d = 0;
+  }
+
+  draw() {
+    this.d += 1;
+    circle(this.x, this.y, this.d);
+  }
 }
 
 function setup() {
@@ -20,32 +21,31 @@ function setup() {
 }
 
 function draw() {
-  background(127,255,212);
+  background(127, 255, 212);
   stroke("white");
   strokeWeight(5);
   noFill();
 
-  for (Let i = 0; i < ripplers.length; i++){
+  for (let i = 0; i < ripplers.length; i++) {
     ripplers[i].draw();
   }
-  
-  //ripple_d += 1;
-  //circle(ripple_x, ripple_y, ripple_d);
 
   //push();
   //fill("white");
   //strokeWeight(1);
   //textSize(50);
-  //text("ripple_x:" + ripple_x, 50, 50);
-  //text("ripple_y:" + ripple_y, 50, 100);
- // pop();
+  //text("ripple_x: " + ripple_x, 50, 50);
+  //text("ripple_y: " + ripple_y, 50, 100);
+  //pop();
 }
 
-function mousePressed(){
-
-ripple_x = mouseX;
-ripple_y = mouseY;
-ripple_d = 0;
-ripplers.push( new Rippler(mouseX, mouseY));
-
+function mousePressed() {
+  ripple_x = mouseX;
+  ripple_y = mouseY;
+  ripple_d = 0;
+  ripplers.push(new Rippler(mouseX, mouseY));
+  
 }
+
+
+setup();
