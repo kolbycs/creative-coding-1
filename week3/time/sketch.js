36509eafe27867bdba974797630fa67ab393982e
kolbycs <ksimpson@mail.umw.edu>
2023-06-04 14:48:33 -0400

    Update sketch.js

--- week3/time/sketch.js
+++ week3/time/sketch.js
@@ -1,7 +1,7 @@
-let x_hour = 50;
-let x_minute = 50;
-let x_second = 50;
-let x_day = 50;
+let y_day = 50; 
+let y_hour = 50; 
+let y_minute = 50;
+let y_second = 50;
 
 function preload(){
   sun = loadImage('https://kolbycs.github.io/creative-coding-true/week3/time/sun.jpg');  //preload sun
@@ -13,7 +13,7 @@ function preload(){
 function setup() {
   createCanvas(1200, 1000); 
 
-sun.resize(500,500);
+sun.resize(600,500);  //use the resize function to create appropriate sizes
 mercury.resize(100,100);
 venus.resize(150,150);
 earth.resize(150,150);
@@ -32,8 +32,8 @@ function draw() {
  
 push();
 
-y_day = map(day(), 0, 23, 150, 350);
-image(sun, 10, y_day, 200);
+y_day = map(day(), 0, 23, 150, 350); //map different units of time to map a vertical path
+image(sun, 10, y_day, 200);    //apply path calculated to the image
 
 y_hour = map(hour(), 0, 59, 150, 350);
 image(mercury, 330, y_hour, 200);
