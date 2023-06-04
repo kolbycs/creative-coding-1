4d269defa5507dcd1d4cdc5c797ccc6fe8602052
kolbycs <ksimpson@mail.umw.edu>
2023-06-04 12:58:04 -0400

    Update sketch.js

--- week3/time/sketch.js
+++ week3/time/sketch.js
@@ -17,8 +17,8 @@ function preload(){
 function setup() {
   createCanvas(1200, 1000); //600, 400
 
-sun.resize(500,500);
-mercury.resize(75,75);
+sun.resize(550,500);
+mercury.resize(50,75);
 venus.resize(150,150);
 earth.resize(150,150);
 
@@ -52,7 +52,7 @@ x_hour = map(hour(), 0, 23, 50, 550);
 image(sun, x_hour, 10, 200);
 
 x_minute = map(minute(), 0, 59, 50, 550);
-image(mercury, x_minute, 500, 200);
+image(mercury, x_minute, 530, 200);
 
 x_second = map(second(), 0, 59, 50, 550);
 image(venus, x_second, 600, 200);
