8a6a2d44382dfc391e3ed8ee4556d55cd31ec3ac
kolbycs <ksimpson@mail.umw.edu>
2023-06-04 12:02:02 -0400

    Update sketch.js

--- week3/time/sketch.js
+++ week3/time/sketch.js
@@ -43,16 +43,16 @@ pop();
 push();
 
 x_hour = map(hour(), 0, 23, 50, 550);
-image(sun, x_hour, 80, 80);
+image(sun, x_hour, 30, 200);
 
 x_minute = map(minute(), 0, 59, 50, 550);
-image(mercury, x_minute, 200, 40);
+image(mercury, x_minute, 200, 100);
 
 x_second = map(second(), 0, 59, 50, 550);
-image(venus, x_second, 300, 40);
+image(venus, x_second, 300, 100);
 
 x_mil = map(mm, 0, 999, 50, 550);
-image(earth, x_mil, 40, 40);
+image(earth, x_mil, 400, 100);
 
 pop();
 
