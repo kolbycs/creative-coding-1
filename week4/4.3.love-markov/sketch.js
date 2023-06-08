55a71b13019acb11269bf2ea1543786514e0800a
kolbycs <ksimpson@mail.umw.edu>
2023-06-08 14:55:55 -0400

    Update sketch.js

--- week4/4.3.love-markov/sketch.js
+++ week4/4.3.love-markov/sketch.js
@@ -1,7 +1,31 @@
+let rm = RiTa.markov(2);
+let data = document.getElementById("source").innerText;
+rm.addText(data);
+
 function setup() {
-  createCanvas(800, 800);
+  
+  let lines = rm.generate(6);
+ 
+  // find the element to add to
+let letter = select("#letter"); 
+
+// add a salutation
+letter.child( createP('My Dearest Harrington,'));
+
+// loop through the array of lines, adding each as a new p element
+for (let l = 0; l < lines.length; l++){
+	let paragraph = createP(lines[l]);
+	letter.child(paragraph);
 }
 
-function draw() {
-  background(220);
+// add a closing
+letter.child( createP('Sincerely,<br>Markov'));
+}
+
+
+function mousePressed(){
+	letter.remove();
+	letter = createDiv();
+	letter.id("letter");
+	setup();
 }
