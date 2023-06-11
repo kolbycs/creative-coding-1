388bed128f61a81c49e7745a52c4e5c72748cf4a
kolbycs <ksimpson@mail.umw.edu>
2023-06-11 19:02:39 -0400

    Update sketch.js

--- week4/book/sketch.js
+++ week4/book/sketch.js
@@ -1,65 +1,60 @@
-/*
-
-This is a p5.js sketch that creates a book by repeating some
-word 50000 times, with punctuation and organization added so 
-that it looks more like a real book.
-
-You are welcome to use this template to make your own 
-repetion-based computer-generated book.
-
-What I Have Done:
- - chosen a word to repeat: quack
- - written the code that repeats that word an arbitrary number of 
-   times, along with punctuation.
-  
-What You Should Do:
- - choose a different word. Animals sounds and screams have already
-   been done. 
- - tweak the book layout options so it looks good.
- - take a look through the code below, which is written in a fairfly
-   verbose way so that you can adjust any aspect you need to. 
-   
-*/
-
-let myBook = "";
+let word = "never odd or even";
+let wc = 0;
 
 function setup() {
- noCanvas();
+  let content = createElement("div");
+  content.attribute("id","content");
   
- let content = createDiv();
-  content.id("content");
+  content.child(createElement("h1","Life, " + word + "."));
+
+  // make a sentence
   
-let bookTitle = createElement("h1","Life, never odd or even");
-
-content.child(bookTitle);
-
-for (let w = 0; w < 50000; w++){
-myBook += random([
-"never odd or even",
-"NEVER ODD OR EVEN",
-"Never odd or even",
-]);
-myBook += random([
-  "",
-  "",
-  "",
-  "",
-  "",
-  ".",
-  ",",
-  "--",
-  "!",
-  "?",
-  "?!"
-]);
-}
-
-let bookContent = createDiv(myBook);
-
-content.child(bookContent);
-
-Bindery.makeBook({
-  content: 'content'
-});
-
+  while( wc < 50000){
+    content.child(createElement("h2",String(word+" ").repeat(random(2,6)).toUpperCase()));
+    
+    for (let p = 0; p < random(5,30); p++){
+      let paragraph = '';
+      for (let s = 0; s < random(3,15); s++){
+        let internalPunct = [",",",",",",";"," --"];
+        let endPunct = [".",".","?","!"];
+        let sentence = word.charAt(0).toUpperCase() + word.slice(1);
+        let sentenceLength = random(9,25);
+        wc += sentenceLength + 1;
+        for (let w = 0; w < sentenceLength; w++){
+          sentence += " " + word;
+          if (random() < 0.2){
+            sentence += random(internalPunct);
+          }
+        }
+        sentence += random(endPunct) + " ";
+        paragraph += sentence;
+      }
+      content.child(createElement("p",paragraph));
+    }
+    
+  
+    
+  }
+  
+  // make a book out of all the content
+  Bindery.makeBook({
+    content: '#content',
+    
+    rules: [
+      Bindery.PageBreak({
+        selector: "h1",
+        position: "after"
+      }),
+    Bindery.PageBreak({
+      selector: 'h2',
+      position: 'before',
+      continue: 'right'
+    }),
+    Bindery.PageBreak({
+      selector: 'p',
+      position: 'after',
+      continue: 'next'
+    }),
+  ]
+  })
 }
