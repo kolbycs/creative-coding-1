a747f47ce8bc09e2958114f7c7f84908794fb289
kolbycs <ksimpson@mail.umw.edu>
2023-06-11 19:08:17 -0400

    Update sketch.js

--- week4/book/sketch.js
+++ week4/book/sketch.js
@@ -1,27 +1,27 @@
-let word = "never odd or even";
+let words =  ["never odd or even", "NEVER ODD OR EVEN", "Never odd or even", "never odd or eveN"];
 let wc = 0;
 
 function setup() {
   let content = createElement("div");
   content.attribute("id","content");
   
-  content.child(createElement("h1","Life, " + word + "."));
+  content.child(createElement("h1","Life, " + words + "."));
 
   // make a sentence
   
   while( wc < 50000){
-    content.child(createElement("h2",String(word+" ").repeat(random(2,6)).toUpperCase()));
+    content.child(createElement("h2",String(words+" ").repeat(random(2,6)).toUpperCase()));
     
     for (let p = 0; p < random(5,30); p++){
       let paragraph = '';
       for (let s = 0; s < random(3,15); s++){
         let internalPunct = [",",",",",",";"," --"];
         let endPunct = [".",".","?","!"];
-        let sentence = word.charAt(0).toUpperCase() + word.slice(1);
+        let sentence = words.charAt(0).toUpperCase() + words.slice(1);
         let sentenceLength = random(9,25);
         wc += sentenceLength + 1;
         for (let w = 0; w < sentenceLength; w++){
-          sentence += " " + word;
+          sentence += " " + words;
           if (random() < 0.2){
             sentence += random(internalPunct);
           }
