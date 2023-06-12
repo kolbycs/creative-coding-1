d70df10f51bddb2a3b854d00a9c8dceb6ae94cbb
kolbycs <ksimpson@mail.umw.edu>
2023-06-12 16:42:02 -0400

    Update sketch.js

--- week4/book/sketch.js
+++ week4/book/sketch.js
@@ -10,9 +10,9 @@ function setup() {
   // make a sentence
   
   while( wc < 50000){
-    content.child(createElement("h2",String(words+" ").repeat(random(2,6)).toUpperCase()));
+    content.child(createElement("h2",String(words+" ").repeat(random(2,8)).toUpperCase()));
     
-    for (let p = 0; p < random(10,50); p++){
+    for (let p = 0; p < random(10,50); p++){ 
       let paragraph = '';
       for (let s = 0; s < random(3,15); s++){
         let internalPunct = [",",",",",",";"," -",":"];
@@ -29,12 +29,15 @@ function setup() {
         sentence += random(endPunct) + " ";
         paragraph += sentence;
       }
-      content.child(createElement("p",paragraph));
+      //content.child(createElement("p",paragraph))
+      content.child(createElement("p").style("font-family", "Georgia").style("font-size", "16px").style("line-height", "1.5").style("text-align", "justify").html(paragraph));
     }
+  }
+    
     
   
     
-  }
+  
   
   // make a book out of all the content
   Bindery.makeBook({
@@ -43,7 +46,9 @@ function setup() {
     rules: [
       Bindery.PageBreak({
         selector: "h1",
-        position: "after"
+        position: "after",
+        continue: "right",
+        header: (info) => `Book Title | Page ${info.pageNumber}`,
       }),
     Bindery.PageBreak({
       selector: 'h2',
