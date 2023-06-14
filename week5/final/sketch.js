ced784efb1bce498a82a57aec9f0d49de1966981
kolbycs <ksimpson@mail.umw.edu>
2023-06-14 12:46:24 -0400

    Update sketch.js

--- week5/final/sketch.js
+++ week5/final/sketch.js
@@ -1,7 +1,22 @@
+let x = 0
+let y = 400
+let xspeed = 1
+function preload(){
+  img = loadImage('https://kolbycs.github.io/creative-coding-true/week5/final/target.jpg');
+}
+
 function setup() {
   createCanvas(800, 800);
 }
 
 function draw() {
-  background(220);
+  background('blue');
+
+  if (x < 0 || x > width){  // detect collosion with walls
+    xspeed = xspeed * -1; // reverse the x direction
+}
+
+x = x + xspeed; // iterate x
+image(img,x-40,y-40,80,80);
+
 }
