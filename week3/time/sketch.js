fe0d5fd65fe30ab65b59b6c685b6b0b6b457d8d7
kolbycs <ksimpson@mail.umw.edu>
2023-06-17 14:14:26 -0400

    Update sketch.js

--- week3/time/sketch.js
+++ week3/time/sketch.js
@@ -12,12 +12,6 @@ function preload(){
 
 function setup() {
   createCanvas(1200, 1000); 
-
-sun.resize(600,500);  //use the resize function to create appropriate sizes
-mercury.resize(100,100);
-venus.resize(150,150);
-earth.resize(150,150);
-
 }
 
 function draw() {
@@ -33,16 +27,16 @@ function draw() {
 push();
 
 y_day = map(day(), 0, 23, 150, 350); //map different units of time to map a vertical path
-image(sun, 10, y_day, 200);    //apply path calculated to the image
+image(sun, 10, y_day,400, 400);    //apply path calculated to the image
 
 y_hour = map(hour(), 0, 59, 150, 350);
-image(mercury, 330, y_hour, 200);
+image(mercury, 430, y_hour, 200, 200);
 
 y_minute = map(minute(), 0, 59, 150, 350);
-image(venus, 600, y_minute, 200);
+image(venus, 700, y_minute, 200, 200);
 
-y_second = map(second(), 0, 31, 150, 350);
-image(earth, 850, y_second, 200);
+y_second = map(second(), 0, 59, 150, 350);
+image(earth, 950, y_second, 200, 200);
 
 pop();
 
